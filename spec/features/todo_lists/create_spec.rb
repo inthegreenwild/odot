653f8798f3afbb22c3043392ea2760bb459ee606
Dylan Cairns <invision33@gmail.com>
2015-05-20 20:17:56 -0500

    refactor todo list creation in spec

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,14 +1,21 @@
 require 'spec_helper'
 
 describe "Creating todo lists" do 
-	it "redirects to the todo list index page on success" do 
+	def create_todo_list(options={})
+		options[:title] ||= "My todo list" #sets a default if nothing is put
+		options[:description] ||= "This is my todo list."
+
 		visit "/todo_lists"
 		click_link "New Todo list"
 		expect(page).to have_content("New Todo List")
 
-		fill_in "Title", with: "My todo list"
-		fill_in "Description", with: "This is what I'm doing today."
+		fill_in "Title", with: options[:title]
+		fill_in "Description", with: options[:description]
 		click_button "Create Todo list"
+	end 
+
+	it "redirects to the todo list index page on success" do 
+		create_todo_list
 
 		expect(page).to have_content("My todo list")
 	end
@@ -17,13 +24,7 @@ describe "Creating todo lists" do
 		expect(TodoList.count).to eq(0) #database is cleared after each test
 										#so there should be no lists
 
-		visit "/todo_lists"
-		click_link "New Todo list"
-		expect(page).to have_content("New Todo List")
-
-		fill_in "Title", with: ""
-		fill_in "Description", with: "This is what I'm doing today."
-		click_button "Create Todo list"
+		create_todo_list(title: "")
 
 		expect(page).to have_content("error")
 		expect(TodoList.count).to eq(0)
@@ -36,13 +37,7 @@ describe "Creating todo lists" do
 		expect(TodoList.count).to eq(0) #database is cleared after each test
 										#so there should be no lists
 
-		visit "/todo_lists"
-		click_link "New Todo list"
-		expect(page).to have_content("New Todo List")
-
-		fill_in "Title", with: "hi"
-		fill_in "Description", with: "This is what I'm doing today."
-		click_button "Create Todo list"
+		create_todo_list(title: "Hi")
 
 		expect(page).to have_content("error")
 		expect(TodoList.count).to eq(0)
@@ -57,13 +52,7 @@ describe "Creating todo lists" do
 		expect(TodoList.count).to eq(0) #database is cleared after each test
 										#so there should be no lists
 
-		visit "/todo_lists"
-		click_link "New Todo list"
-		expect(page).to have_content("New Todo List")
-
-		fill_in "Title", with: "Grocery List"
-		fill_in "Description", with: ""
-		click_button "Create Todo list"
+		create_todo_list(title: "Grocery List",description: "")
 
 		expect(page).to have_content("error")
 		expect(TodoList.count).to eq(0)
@@ -72,17 +61,11 @@ describe "Creating todo lists" do
 		expect(page).to_not have_content("Grocery List")
 	end 
 
-	it "displays an error when the todo list has a description < 3 characters" do
+	it "displays an error when the todo list has a description < 5 characters" do
 		expect(TodoList.count).to eq(0) #database is cleared after each test
 										#so there should be no lists
 
-		visit "/todo_lists"
-		click_link "New Todo list"
-		expect(page).to have_content("New Todo List")
-
-		fill_in "Title", with: "Grocery List"
-		fill_in "Description", with: "food"
-		click_button "Create Todo list"
+		create_todo_list(title: "Grocery List", description: "food")
 
 		expect(page).to have_content("error")
 		expect(TodoList.count).to eq(0)
