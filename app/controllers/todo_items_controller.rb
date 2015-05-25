d94f2735f7a9578b1585c5cfa9959e2efd857635
Dylan <invision33@gmail.com>
2015-05-24 19:24:20 -0500

    write tests for todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,4 +1,6 @@
 class TodoItemsController < ApplicationController
   def index
+  	#initialized variable "@" that gives us access to it in our view
+  	@todo_list = TodoList.find(params[:todo_list_id])
   end
 end
